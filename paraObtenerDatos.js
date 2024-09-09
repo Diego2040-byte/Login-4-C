5a9763fe8f3642fc8bfa6ca22a18f5a4e5b25646
Diego <diegotamay2040@gmail.com>
2024-09-09 16:10:47 -0600

    Agregue js para obtner la info de los inputs

---
+++ paraObtenerDatos.js
@@ -0,0 +1,9 @@
+function datos(){
+
+    let nombre=document.getElementById('Usuario').value;
+    let contra=document.getElementById('contra').value;
+
+    console.log("Nombre:"+ nombre);
+    console.log("contraseña"+ contra);
+
+}
